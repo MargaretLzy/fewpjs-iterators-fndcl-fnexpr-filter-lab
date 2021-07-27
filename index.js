4b7d19a43d2a957e578c566f6d076a3281d4e266
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2021-07-27 16:16:18 -0400

    commit tree f5f395b51df9734cf6693422661ad704367335f5

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driver, string){
+return driver.filter(d=>
+    d.toLowerCase()===string.toLowerCase()
+)
+}
+
+function fuzzyMatch(driver, string){
+    return driver.filter(d=>
+        d.substring(0,2)===string
+    )
+}
+
+function matchName(driver, string){
+    return driver.filter(d=>
+        d.name===string
+    )
+}
